5ea03e8a4d70d73fb5ba09e2a8b480dd5f275a68
ChuangAsiaS2 <for.omar09@gmail.com>
2025-02-03 03:03:58 +0700

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -87,10 +87,10 @@ function convertCSVArrayToTraineeData(csvArrays) {
     trainee.affiliation = traineeArray[3];
     trainee.nationality = traineeArray [4];
     trainee.rating = traineeArray[5];
-    trainee.birthyear = traineeArray[6];
-    trainee.eliminated = traineeArray[7] === 'e'; // sets trainee to be eliminated if 'e' appears in 7th col
+    trainee.birthyear = traineeArray[5];
+    trainee.eliminated = traineeArray[6] === 'e'; // sets trainee to be eliminated if 'e' appears in 7th col
     trainee.top7 = traineeArray[7] === 't'; // sets trainee to top 7 if 't' appears in 7th column
-    trainee.id = parseInt(traineeArray[8]) - 1; // trainee id is the original ordering of the trainees in the first csv
+    trainee.id = parseInt(traineeArray[7]) - 1; // trainee id is the original ordering of the trainees in the first csv
     trainee.image =
       trainee.stage_name.replaceAll(" ", "", ".").replaceAll("-", "", "_") + ".png";
     return trainee;
